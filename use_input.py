0993b29a7617077e87af47e0d41d42db9cd708d9
malilishaddy <malilishaddy@gmail.com>
2024-02-24 18:08:19 +0300

    Second commit

---
+++ use_input.py
@@ -0,0 +1,4 @@
+name = input("What's your name?")
+age = input("What's your age?")
+location = input("Where do you live?")
+print("Hello, " + name + "you are " + age+" years old and live in "+location)
